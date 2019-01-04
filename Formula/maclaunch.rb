9d7cdb4a85114a856100238835547bb87d40d6c2
HazCod <HazCod@users.noreply.github.com>
2019-01-04 07:37:36 +0100

    Add maclaunch formula

---
+++ Formula/maclaunch.rb
@@ -0,0 +1,15 @@
+class Maclaunch < Formula
+  desc "Manage your macOS startup items."
+  homepage "https://github.com/hazcod/macluanch"
+  url "https://codeload.github.com/HazCod/maclaunch/zip/0.8"
+  version "0.8"
+  sha256 "08b17c4a7dcb4d221530eabe483066222275cf3e5f429274f4bf0838fe40561e"
+
+  def install
+    bin.install "maclaunch.sh" => "maclaunch"
+  end
+
+  test do
+    system "#{bin}/maclaunch"
+  end
+end
