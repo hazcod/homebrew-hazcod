d3b9ee7306222a7f575d2f7bb5cfc4b7f01b280f
HazCod <HazCod@users.noreply.github.com>
2019-01-21 09:39:25 +0100

    Add restimac

---
+++ Formula/restimac.rb
@@ -0,0 +1,14 @@
+class RestiMac < Formula
+  desc "Triggers a restic backup on your mac when you're connected to a specific Wireless Network. "
+  homepage "https://github.com/HazCod/restimac.sh"
+  url "https://codeload.github.com/HazCod/restimac.sh/zip/1.0"
+  version "1.0"
+  sha256 "2d658213881507fe4623400f44f81131a2161b5086b1ac5fe5497974e99dfc76"
+  def install
+    bin.install "restimac.sh" => "restimac"
+  end
+
+  test do
+    system "#{bin}/restimac"
+  end
+end
