f571c0f3bfa8fa0783ce00868874a82e2ea01b38
Niels Hofmans <hello@ironpeak.be>
2019-12-27 09:36:52 +0100

    maclaunch: Fix typo

--- Formula/maclaunch.rb
+++ Formula/maclaunch.rb
@@ -2,7 +2,7 @@ class Maclaunch < Formula
   desc "Manage your macOS startup items."
   homepage "https://github.com/hazcod/maclaunch"
   url "https://codeload.github.com/HazCod/maclaunch/zip/0.9"
-  version "0.0"
+  version "0.9"
   sha256 "daaac1aa3f95b5f4f354b626a89fed7da31e9af6d175bc89f6afd8864580f954"
 
   def install
