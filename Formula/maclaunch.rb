b5eaf7e7fdf4672296fc8ce9f97b6ba0954f6ed5
Niels Hofmans <hello@ironpeak.be>
2020-12-03 13:41:53 +0100

    fix(maclaunch): upgrade to 2.0

--- Formula/maclaunch.rb
+++ Formula/maclaunch.rb
@@ -1,9 +1,9 @@
 class Maclaunch < Formula
   desc "Manage your macOS startup items."
   homepage "https://github.com/hazcod/maclaunch"
-  url "https://codeload.github.com/HazCod/maclaunch/zip/1.0"
-  version "1.0"
-  sha256 "870174b841f215f62a7e07bab6c56ed6e6c0ff02a094f99f86483b33ee78fb9a"
+  url "https://codeload.github.com/HazCod/maclaunch/zip/2.0"
+  version "2.0"
+  sha256 "c4933159b8a80ee0d2295093710db178a847525824b03e28e7ec2e59351fa7c1"
 
   def install
     bin.install "maclaunch.sh" => "maclaunch"
