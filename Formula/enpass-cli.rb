bc9bb8e0bedc186212ea53ccc921998e7c4fe891
Niels Hofmans <hello@ironpeak.be>
2020-12-08 15:11:57 +0100

    feat: add enpasscli

---
+++ Formula/enpass-cli.rb
@@ -0,0 +1,11 @@
+class EnpassCLI < Formula
+  desc "Commandline client for the Enpass 6 password manager."
+  homepage "https://github.com/hazcod/enpass-cli"
+  url "https://github.com/hazcod/enpass-cli/releases/download/v1.0/enpass-cli_1.0_darwin_amd64.tar.gz"
+  version "1.0"
+  sha256 "55c1f6a5e0f0bb68fefc1b9103ae7108ab26d177a3d90888798fbada97d9eea6"
+
+  test do
+    system "#{bin}/enpasscli"
+  end
+end
