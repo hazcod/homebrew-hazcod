3a5dcf1b4f899fcfd46a59083210de2ae102751b
Niels Hofmans <hello@ironpeak.be>
2021-02-13 23:53:13 +0100

    feat(macbac): version 1.1

--- Formula/macbac.rb
+++ Formula/macbac.rb
@@ -1,9 +1,9 @@
 class Macbac < Formula
   desc "Manage your macOS snapshots."
   homepage "https://github.com/hazcod/macbac"
-  url "https://codeload.github.com/hazcod/macbac/zip/1.0"
-  version "1.0"
-  sha256 "60114153290a5f5a7743e9253b578c49807ab5c994c6ee1d1f4cd4ce5dea98ed"
+  url "https://codeload.github.com/hazcod/macbac/zip/1.1"
+  version "1.1"
+  sha256 "d84147df6e9b43a3d3408c10c303a9f36baf0f491f12691b3c9604a8335be6ca"
 
   def install
     bin.install "macbac.sh" => "macbac"
