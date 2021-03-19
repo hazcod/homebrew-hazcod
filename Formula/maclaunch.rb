ccb5280b573fe5f7d3f715bf4a61c8b796a26d54
Niels Hofmans <hello@ironpeak.be>
2021-03-19 08:09:56 +0100

    feat: maclaunch 2.2.0

--- Formula/maclaunch.rb
+++ Formula/maclaunch.rb
@@ -1,9 +1,9 @@
 class Maclaunch < Formula
   desc "Manage your macOS startup items."
   homepage "https://github.com/hazcod/maclaunch"
-  url "https://codeload.github.com/HazCod/maclaunch/zip/2.1"
-  version "2.1"
-  sha256 "6bd122d06235cd4540a8c547678201a7e851453dac5e5f8887cb767721558237"
+  url "https://codeload.github.com/HazCod/maclaunch/zip/2.2.0"
+  version "2.2.0"
+  sha256 "1fc40a78873395db53d0b8dc40a20377db35a6c646f4f93624d10f6113850162"
 
   def install
     bin.install "maclaunch.sh" => "maclaunch"
