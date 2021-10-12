996f101a5009370e94f0d544e87c965ba4641ec2
Niels Hofmans <niels@ironpeak.be>
2021-10-12 09:04:23 +0200

    Update macbac.rb

--- Formula/macbac.rb
+++ Formula/macbac.rb
@@ -1,7 +1,7 @@
 class Macbac < Formula
   desc "Manage your macOS snapshots."
   homepage "https://github.com/hazcod/macbac"
-  url "https://codeload.github.com/hazcod/macbac/zip/1.1"
+  url "https://codeload.github.com/hazcod/macbac/zip/1.2"
   version "1.2"
   sha256 "f5ace4071aeb39397145d28e6221c193d62e048361a9a2b06b81b96703bf254a"
 
