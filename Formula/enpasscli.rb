24eff2554513d950c34e0def2789e10ff35a0408
Niels Hofmans <niels@ironpeak.be>
2024-03-13 20:47:47 +0100

    feat(enpass): update to 1.6.1

--- Formula/enpasscli.rb
+++ Formula/enpasscli.rb
@@ -1,9 +1,9 @@
 class Enpasscli < Formula
   desc "Commandline client for the Enpass 6 password manager."
   homepage "https://github.com/hazcod/enpass-cli"
-  url "https://github.com/hazcod/enpass-cli/releases/download/v1.0/enpass-cli_1.0_darwin_amd64.tar.gz"
-  version "1.0"
-  sha256 "55c1f6a5e0f0bb68fefc1b9103ae7108ab26d177a3d90888798fbada97d9eea6"
+  url "https://github.com/hazcod/enpass-cli/releases/download/v1.6.1/enpass-cli_1.6.1_darwin_arm64.zip"
+  version "1.6.1"
+  sha256 "55c53956e277dd3572c9ccad91089fe44a2240b2d20c24e2e6b9f0ed95ae12c0"
 
   test do
     system "#{bin}/enpasscli"
